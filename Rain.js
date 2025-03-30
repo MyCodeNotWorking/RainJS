bd86a273d2a24bc9a09e6f672f417d15d8fab4f5
MyCodeNotWorking <126179690+MyCodeNotWorking@users.noreply.github.com>
2025-03-30 16:09:15 +0200

    Made renderer functional

--- Rain.js
+++ Rain.js
@@ -1,29 +1,58 @@
 class RainBody {
-  constructor() {} //add properties like mass, verticies, color etc. This is a rigid body
+  constructor(options = {}) {
+    this.fillColor = options.fillColor || "red";
+    this.borderWidth = options.borderWidth || 0.05;
+    this.borderColor = options.borderColor || "black";
+    this.vertices = options.vertices || [{x: 1, y: 1}, {x: 1.5, y: 1}, {x: 1.5, y: 1.5}, {x: 1, y: 1.5}];
+  } // units of measurement is in meters, this is a rigidbody
 }
 
 class RainScene {
-  constructor() {
+  constructor(options = {}) {
     this.objects = [];
-    //add properties like gravity or sleep
+    this.backgroundColor = options.backgroundColor || "lightblue";
+    //add properties like gravity and sleep so that the scene can be paused
   }
   
   update() {
     // ... 
   }
 
-  addObject() {
-    // ...
+  addBody(body) {
+    this.objects.push(body);
   }
 }
 
 class RainRenderer {
-  constructor() {
-    this.canvas = null;
+  constructor(options = {}) {
+    this.canvas = options.canvas || null;
+    this.ctx = options.ctx || null;
+    this.scale = options.scale || 100;
   }
   
   render(scene) {
-    // ... 
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = scene.backgroundColor;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.save();
+    this.ctx.scale(this.scale, this.scale);
+
+    for (var i = 0; i < scene.objects.length; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(scene.objects[i].vertices[0].x, scene.objects[i].vertices[0].y);
+      for (var j = 1; j < scene.objects[i].vertices.length; j++) {
+          this.ctx.lineTo(scene.objects[i].vertices[j].x, scene.objects[i].vertices[j].y);
+      }
+      this.ctx.closePath();
+      this.ctx.strokeStyle = scene.objects[i].borderColor;
+      this.ctx.lineWidth = scene.objects[i].borderWidth;
+      this.ctx.fillStyle = scene.objects[i].fillColor;
+      this.ctx.fill();
+      this.ctx.stroke();
+    }
+
+    this.ctx.restore();
   }
 }
 
--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 canvas.width = 720;
 canvas.height = 480;
+
+var rain = new Rain();
+var world = new rain.world();
+var renderer = new rain.renderer({
+    canvas: canvas,
+    ctx: ctx
+});
+world.renderer = renderer;
+
+var scene1 = new rain.scene();
+world.scene = scene1;
+
+var testBody = new rain.body();
+scene1.addBody(testBody);
+
+renderer.render(scene1);
+console.log(scene1.objects);
